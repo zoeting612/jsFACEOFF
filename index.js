b1d61fa5bf2a56c871704d8e34a1280f6110df02
Zoe <tingz@bc.edu>
2020-11-12 19:28:18 -0500

    Experimenting with different way to read csv data in js

--- index.js
+++ index.js
@@ -1,65 +1,31 @@
-class subject{
-    set ActorGender(ActorGender){
-        this._ActorGender=ActorGender;
-    }
-    set ActorRace(ActorRace){
-        this._ActorRace=ActorRace;
-    }
-    set ActorID(ActorID){
-        this._ActorID=ActorID;
-    }
-    set Directory(Directory){
-        this._Directory=Directory;
-    }
-    set Filename(Filename){
-        this._Filename=Filename;
-    }
-    set ExpressionIntensity(ExpressionIntensity){
-        this._ExpressionIntensity=ExpressionIntensity;
-    }
-    set TestImage(TestImage){
-        this._TestImage=TestImage;
-    }
-    get ActorGender(){
-        return this._ActorGender;
-    }
-    get ActorRace(){
-        return this._ActorRace;
-    }
-    get ActorID(){
-        return this._ActorID;
-    }
-    get Directory(){
-        return this._Directory;
-    }
-    get Filename(){
-        return this._Filename;
-    }
-    get ExpressionIntensity(){
-        return this._ExpressionIntensity;
-    }
-    get TestImage(){
-        return this._TestImage;
-    }
-    constructor(){
-    }
+var fs = require("fs");
+var parse = require("csv-parse");
+
+var csvFile = "TestData.csv";
+
+class User {
+  constructor(ActorGender,ActorRace,ActorID,Directory,Filename,ExpressionIntensity,TestImage) {
+    this.ActorGender = ActorGender;
+    this.ActorRace = ActorRace;
+    this.ActorID = ActorID;
+    this.Directory = Directory;
+    this.Filename = Filename;
+    this.ExpressionIntensity = ExpressionIntensity;
+    this.TestImage = TestImage;
+  }
 }
-let subs=[];// Array to store subject Objects
-const csv=require('csvtojson')
-// Invoking csv returns a promise
-const converter=csv()
-.fromFile('./TestData.csv')
-.then((json)=>{
-    let e;
-    json.forEach((row)=>{
-        e=new Subject();// New Subject Object
-        Object.assign(e,row);// Assign json to the new Subject
-        emp.push(e);// Add the Subject to the Array
-        
-    });
-}).then(()=>{
-    // Output the names of the Subjects
-    emp.forEach((em)=>{
-        console.log(em.Name);// Invoke the Name getter
-    });
-});
+
+const processData = (err, data) => {
+  if (err) {
+    console.log(`An error was encountered: ${err}`);
+    return;
+  }
+
+  data.shift(); // only required if csv has heading row
+
+  const userList = data.map(row => new User(...row));
+  //analyseUsers(userList);
+}
+
+fs.createReadStream(csvFile)
+  .pipe(parse({ delimiter: ',' }, processData));
