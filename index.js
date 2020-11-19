7c4e9ccf90314dda8b8a3baa0438583b8ce0d045
Zoe <tingz@bc.edu>
2020-11-19 15:45:54 -0500

    New functions

--- index.js
+++ index.js
@@ -24,7 +24,23 @@ const processData = (err, data) => {
   data.shift(); // only required if csv has heading row
 
   const userList = data.map(row => new User(...row));
+  /* prints the gender of the first subject*/
+  console.log(userList[0].ActorGender)
   //analyseUsers(userList);
+  
+  /* processes which images are test images and returns a list of their paths*/
+  function perception_images(){
+    var test_images = []
+    for (var i = 0; i<userList.length; i++){
+      if (userList[i].TestImage=="yes"){
+        test_images.push(userList[i].Directory+userList[i].Filename)
+      }
+    }
+    return test_images
+  }
+  //test this function
+  var images = perception_images()
+  console.log(images)
 }
 
 fs.createReadStream(csvFile)
